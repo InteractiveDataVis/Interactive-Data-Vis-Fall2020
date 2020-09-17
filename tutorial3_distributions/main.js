0ac27a236f7136955abbba74fa5663b5cbd80431
efrymire <ellie.frymire@gmail.com>
2020-09-16 21:03:47 -0400

    class week 3

--- tutorial3_distributions/main.js
+++ tutorial3_distributions/main.js
@@ -4,8 +4,6 @@ const width = window.innerWidth * 0.7,
   margin = { top: 20, bottom: 50, left: 60, right: 40 },
   radius = 5;
 
-// these variables allow us to access anything we manipulate in init() but need access to in draw().
-// All these variables are empty before we assign something to them.
 let svg;
 let xScale;
 let yScale;
@@ -13,62 +11,71 @@ let yScale;
 /* APPLICATION STATE */
 let state = {
   data: [],
-  selection: "All" // + YOUR FILTER SELECTION
+  selection: "All"
 };
 
 /* LOAD DATA */
 d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
-  // + SET YOUR DATA PATH
   console.log("raw_data", raw_data);
   state.data = raw_data;
+  console.log("state", state)
   init();
 });
 
 /* INITIALIZING FUNCTION */
-// this will be run *one time* when the data finishes loading in 
 function init() {
-  // + SCALES
-
-  // + AXES
-
-  // + UI ELEMENT SETUP
 
   const selectElement = d3.select("#dropdown").on("change", function() {
-    // `this` === the selectElement
-    // 'this.value' holds the dropdown value a user just selected
-
     state.selection = this.value
     console.log("new value is", this.value);
-    draw(); // re-draw the graph based on this new selection
+    console.log(state)
+    draw();
   });
 
-  // add in dropdown options from the unique values in the data
   selectElement
     .selectAll("option")
-    .data(["All", "1", "2", "3"]) // + ADD UNIQUE VALUES
+    .data(["All", "D", "R", "I"])
     .join("option")
     .attr("value", d => d)
     .text(d => d);
 
-  // + CREATE SVG ELEMENT
+  svg = d3.select("#d3-container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
 
-  // + CALL AXES
+  xScale = d3.scaleLinear()
+    .domain([0, 1])
+    .range([0, width])
 
-  draw(); // calls the draw function
+  yScale = d3.scaleLinear()
+    .domain([0, 100])
+    .range([height, 0])
+
+  draw();
 }
 
 /* DRAW FUNCTION */
- // we call this everytime there is an update to the data/state
 function draw() {
-  
-  // + FILTER DATA BASED ON STATE
 
-  // const dot = svg
-  //   .selectAll("circle")
-  //   .data(filteredData, d => d.name)
-  //   .join(
-  //     enter => enter, // + HANDLE ENTER SELECTION
-  //     update => update, // + HANDLE UPDATE SELECTION
-  //     exit => exit // + HANDLE EXIT SELECTION
-  //   );
+  let filteredData = state.data
+  if (state.selection !== "All") {
+    filteredData = state.data.filter(d => d.party === state.selection)
+  }
+
+  console.log(filteredData)
+
+  const dot = svg.selectAll(".dot")
+    .data(filteredData, d => d.name)
+    .join(
+      enter => enter
+      .append("circle")
+      .attr("class", "dot")
+      .attr("cx", d => xScale(d["ideology_rating"]))
+      .attr("cy", d => yScale(d["environmental_rating"]))
+      .attr("r", 10),
+      update => update,
+      exit => exit.remove()
+    )
+
 }
