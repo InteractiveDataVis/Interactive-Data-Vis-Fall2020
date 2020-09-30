c447e77c4f968fae1145714f8238a0c8d1981da9
Meriem <meriem.benarbia@gmail.com>
2020-09-30 10:42:13 -0400

    assignement03done

--- tutorial4_timeseries/main.js
+++ tutorial4_timeseries/main.js
@@ -1,6 +1,7 @@
 /* CONSTANTS AND GLOBALS */
-const width = window.innerWidth * 0.7,
-  height = window.innerHeight * 0.7,
+
+const height = window.innerHeight * 0.7,
+width = window.innerWidth * 0.7,
   margin = { top: 20, bottom: 50, left: 60, right: 40 },
   radius = 5;
 
